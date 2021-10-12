caf3e3faf2d4cee143ffea15082385ec78008d53
Mia <84252026+miadao@users.noreply.github.com>
2021-10-12 19:32:40 -0400

    c

--- backend/api/utils.js
+++ backend/api/utils.js
@@ -1,1 +1,50 @@
+//SAMPLE util functions below for userLoggedIn and requiredNotSent
+
+// function userLoggedIn(req, res, next) {
+//     if (!req.user) {
+//       res.status(401)
+//       next({
+//         name: "MissingUserError",
+//         message: "You must be logged in to perform this action"
+//       });
+//     } 
+//     next();
+//   }
+  
+//   const requiredNotSent = ({ requiredParams, atLeastOne = false }) => {
+//     return (req, res, next) => {
+//       if(atLeastOne) {
+//         let numParamsFound = 0;
+//         for(let param of requiredParams) {
+//           if(req.body[param] !== undefined) {
+//             numParamsFound++;
+//           }
+//         }
+//         if(!numParamsFound) {
+//           next({
+//             name: 'MissingParams',
+//             message: `Must provide at least one of these in body: ${requiredParams.join(', ')}`
+//           })
+//         } else {
+//           next();
+//         }
+//       } else {
+//         const notSent = [];
+//         for(let param of requiredParams) {
+//           if(req.body[param] === undefined) {
+//             notSent.push(param);
+//           }
+//         }
+//         if(notSent.length) next({
+//           name: 'MissingParams',
+//           message: `Required Parameters not sent in body: ${notSent.join(', ')}`
+//         })
+//         next();
+//       }
+//     }
+//   }
+//   module.exports = {
+//     userLoggedIn,
+//     requiredNotSent
+//   }
 
