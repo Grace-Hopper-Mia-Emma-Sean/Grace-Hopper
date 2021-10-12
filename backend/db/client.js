ea807cd46da938fce79f8590669248972bf5094a
Mia <84252026+miadao@users.noreply.github.com>
2021-10-12 19:33:06 -0400

    c

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,1 +1,10 @@
+const { Client } = require('pg') 
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/capstone'
+const client = new Client(
+    connectionString
+);
+
+module.exports = client
+
+//TO DO: be sure to do PostgreSQL in terminal and create the database there first. Name database as "capstone" for the link in line 2.
 
