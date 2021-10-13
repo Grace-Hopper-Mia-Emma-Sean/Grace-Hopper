8aa4cefeda821a70cfe8ffaaffb4e990ac3f7ecb
Mia Dao <miadao@Mias-MacBook-Pro.local>
2021-10-12 21:24:50 -0400

    project started

--- backend/db/seedData.js
+++ backend/db/seedData.js
@@ -1,7 +1,8 @@
 const client = require('./client');
 
 const {
-  //TO DO: call db components
+  //TO DO: call db components as they get created 
+
 } = require('./index')
 
 async function dropTables() {
@@ -10,15 +11,18 @@ async function dropTables() {
     console.log('Dropping All Tables...');
 
     await client.query(`
-    // TO DO
-
-
-
-
-
-
-
-
+    DROP TABLE IF EXISTS payment_details;
+    DROP TABLE IF EXISTS order_items;
+    DROP TABLE IF EXISTS order_details;
+    DROP TABLE IF EXISTS product_discount;
+    DROP TABLE IF EXISTS product_inventory;
+    DROP TABLE IF EXISTS product_category;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS user_payment;
+    DROP TABLE IF EXISTS user_address;
+    DROP TABLE IF EXISTS cart_items;
+    DROP TABLE IF EXISTS shopping_session;
+    DROP TABLE IF EXISTS users;
     `);
     console.log('Finished dropping tables!')
   }catch (error) {
@@ -130,7 +134,6 @@ async function createTables() {
       status BOOLEAN DEFAULT false
     );
 
-
     `);
   } catch (error) {
     console.error("Error dropping table!")
@@ -158,121 +161,43 @@ async function createInitialUsers() {
   }
 }
 //SAMPLE BELOW:
-// async function createInitialActivities() {
+// async function createInitialProducts() {
 //   try {
-//     console.log('Starting to create activities...');
+//     console.log('Starting to create products...');
 
-//     const activitiesToCreate = [
-//       { name: 'wide-grip standing barbell curl', description: 'Lift that barbell!' },
-//       { name: 'Incline Dumbbell Hammer Curl', description: 'Lie down face up on an incline bench and lift thee barbells slowly upward toward chest' },
-//       { name: 'bench press', description: 'Lift a safe amount, but push yourself!' },
-//       { name: 'Push Ups', description: 'Pretty sure you know what to do!' },
-//       { name: 'squats', description: 'Heavy lifting.' },
-//       { name: 'treadmill', description: 'running' },
-//       { name: 'stairs', description: 'climb those stairs' },
+//     const productsToCreate = [
+//       //TO DO
 //     ]
-//     const activities = await Promise.all(activitiesToCreate.map(createActivity));
+//     const activities = await Promise.all(productsToCreate.map(createProducts));
 
-//     console.log('activities created:');
-//     console.log(activities);
+//     console.log('Products created:');
+//     console.log(products);
 
-//     console.log('Finished creating activities!');
+//     console.log('Finished creating products!');
 //   } catch (error) {
-//     console.error('Error creating activities!');
+//     console.error('Error creating products!');
 //     throw error;
 //   }
 // }
 
 //SAMPLE BELOW
-// async function createInitialRoutines() {
+// async function createInitialUsers() {
 //   try {
-//     console.log('starting to create routines...');
+//     console.log('starting to create users...');
 
-//     const routinesToCreate = [
-//       {creatorId: 2, isPublic: false, name: 'Bicep Day', goal: 'Work the Back and Biceps.'},
-//       {creatorId: 1, isPublic: true, name: 'Chest Day', goal: 'To beef up the Chest and Triceps!'},
-//       {creatorId: 1, isPublic: false, name: 'Leg Day', goal: 'Running, stairs, squats'},
-//       {creatorId: 2, isPublic: true, name: 'Cardio Day', goal: 'Running, stairs. Stuff that gets your heart pumping!'},
+//     const usersToCreate = [
+//       //TO DO
 //     ]
-//     const routines = await Promise.all(routinesToCreate.map(routine => createRoutine(routine)));
-//     console.log('Routines Created: ', routines)
-//     console.log('Finished creating routines.')
+//     const routines = await Promise.all(usersToCreate.map(routine => createUser(users)));
+//     console.log('Users Created: ', users)
+//     console.log('Finished creating users.')
 //   } catch (error) {
 //     throw error;
 //   }
 // }
 
+// ADD MORE CREATION AS NEEDED
 
-//SAMPLE BELOW:
-// async function createInitialRoutineActivities() {
-//   try {
-//     console.log('starting to create routine_activities...');
-//     const [bicepRoutine, chestRoutine, legRoutine, cardioRoutine] = await getRoutinesWithoutActivities();
-//     const [bicep1, bicep2, chest1, chest2, leg1, leg2, leg3] = await getAllActivities();
-
-//     const routineActivitiesToCreate = [
-//       {
-//         routineId: bicepRoutine.id,
-//         activityId: bicep1.id,
-//         count: 10,
-//         duration: 5 
-//       },
-//       {
-//         routineId: bicepRoutine.id,
-//         activityId: bicep2.id,
-//         count: 10,
-//         duration: 8 
-//       },
-//       {
-//         routineId: chestRoutine.id,
-//         activityId: chest1.id,
-//         count: 10,
-//         duration: 8 
-//       },
-//       {
-//         routineId: chestRoutine.id,
-//         activityId: chest2.id,
-//         count: 10,
-//         duration: 7 
-//       },
-//       {
-//         routineId: legRoutine.id,
-//         activityId: leg1.id,
-//         count: 10,
-//         duration: 9 
-//       },
-//       {
-//         routineId: legRoutine.id,
-//         activityId: leg2.id,
-//         count: 10,
-//         duration: 10 
-//       },
-//       {
-//         routineId: legRoutine.id,
-//         activityId: leg3.id,
-//         count: 10,
-//         duration: 7 
-//       },
-//       {
-//         routineId: cardioRoutine.id,
-//         activityId: leg2.id,
-//         count: 10,
-//         duration: 10 
-//       },
-//       {
-//         routineId: cardioRoutine.id,
-//         activityId: leg3.id,
-//         count: 10,
-//         duration: 15 
-//       },
-//     ]
-//     const routineActivities = await Promise.all(routineActivitiesToCreate.map(addActivityToRoutine));
-//     console.log('routine_activities created: ', routineActivities)
-//     console.log('Finished creating routine_activities!')
-//   } catch (error) {
-//     throw error;
-//   }
-// }
 
 //SAMPLE rebuildDB() below:
 // async function rebuildDB() {
@@ -292,7 +217,7 @@ async function createInitialUsers() {
   // finally {
   //   client.end()
   // }
-}
+// }
 
 module.exports = { 
   rebuildDB 
