b3c2de4840d4f55402bfcc95277fc511dea28dc1
emma-elise <loirratse@gmail.com>
2021-10-13 08:15:47 -0400

    set up api/routers pages and added to api index

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,44 @@
-//TO DO: export URL for our database 
+//TO DO: export URL for our database
 //SAMPLE: export const BASE_URL = 'https://fitnesstrac-kr.herokuapp.com/api';
 //UTILIZE this BASE_URL throughout React framework
 
+const express = require("express");
+const apiRouter = express.Router();
+
+const jwt = require("jsonwebtoken");
+// TODO: change getUserById import once names are finalized
+// const { getUserById } = require("../db/adapters/users");
+const { JWT_SECRET } = process.env;
+
+// TODO: Tweak / comment back in when ready to use
+// apiRouter.use(async (req, res, next) => {
+//   const prefix = "Bearer ";
+//   const auth = req.header("Authorization");
+//   if (!auth) {
+//     next();
+//   } else if (auth.startsWith(prefix)) {
+//     const token = auth.slice(prefix.length);
+//     try {
+//       const { id } = jwt.verify(token, JWT_SECRET);
+//       if (id) {
+//         req.user = await getUserById(id);
+//         next();
+//       }
+//     } catch ({ name, message }) {
+//       next({ name, message });
+//     }
+//   } else {
+//     next({
+//       name: "AuthorizationHeaderError",
+//       message: `Authorization token must start with ${prefix}`,
+//     });
+//   }
+// });
+
+// TODO: Change routes once we're ready
+apiRouter.use("/health", require("./routers"));
+apiRouter.use("/orders", require("./routers"));
+apiRouter.use("/products", require("./routers"));
+apiRouter.use("/users", require("./routers"));
+
+module.exports = apiRouter;
---
+++ frontend/src/api/routers/index.js
@@ -0,0 +1,5 @@
+export { healthRouter } from "./health";
+export { ordersRouter } from "./orders";
+export { productsRouter } from "./products";
+export { usersRouter } from "./users";
+export {} from "./utils";
