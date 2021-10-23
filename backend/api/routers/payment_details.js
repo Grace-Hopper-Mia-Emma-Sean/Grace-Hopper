0abd621f24bb80173f5ee521733704d18163f64a
Mia Dao <miadao@Mias-MacBook-Pro.local>
2021-10-23 11:22:38 -0400

    took out middleware in payment details

--- backend/api/routers/payment_details.js
+++ backend/api/routers/payment_details.js
@@ -12,7 +12,7 @@ const {
 
 const {authenticate, admin} = require('../utils')
 
-paymentDetailsRouter.post("/", authenticate, admin, async (req, res, next) => {
+paymentDetailsRouter.post("/",async (req, res, next) => {
   const { order_id, amount, provider, status } = req.body;
   try {
     const createdPaymentDetails = await createPaymentDetails({
@@ -27,7 +27,7 @@ paymentDetailsRouter.post("/", authenticate, admin, async (req, res, next) => {
   }
 });
 
-paymentDetailsRouter.get("/", authenticate, admin, async (req, res, next) => {
+paymentDetailsRouter.get("/", async (req, res, next) => {
   try {
     const allPaymentDetails = await getAllPaymentDetails();
     res.send(allPaymentDetails);
@@ -36,7 +36,7 @@ paymentDetailsRouter.get("/", authenticate, admin, async (req, res, next) => {
   }
 });
 
-paymentDetailsRouter.patch('/:paymentDetailsId', authenticate, admin, async (req, res, next) => {
+paymentDetailsRouter.patch('/:paymentDetailsId', async (req, res, next) => {
     const { order_id, amount, provider, status } = req.body;
     const { paymentDetailsId } = req.params;
     const updateFields = {id: paymentDetailsId, order_id, amount, provider, status}
@@ -61,7 +61,7 @@ paymentDetailsRouter.patch('/:paymentDetailsId', authenticate, admin, async (req
 });
 
 
-paymentDetailsRouter.delete("/:paymendDetailsId", authenticate, admin, async (req, res, next) => {
+paymentDetailsRouter.delete("/:paymendDetailsId", async (req, res, next) => {
   const { paymendDetailsId } = req.params;
   try {
     const deletePaymentDetails = await destroyPaymentDetails(paymendDetailsId);
