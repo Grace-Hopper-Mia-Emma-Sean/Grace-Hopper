a886740ff8b7eb5d8922b7917be18cc0f0eb4a63
Mia <84252026+miadao@users.noreply.github.com>
2021-10-27 15:37:22 -0400

    Updated index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -9,17 +9,18 @@ export { OrderItems } from "./orders/OrderItems";
 export { PaymentDetails } from "./payment/PaymentDetails";
 export { UserPayment } from "./payment/UserPayment";
 export { CreateOrderDetails } from "./orders/CreateOrderDetails";
-export { UsersTable } from "./users/UsersTable";
 export { CreateOrderItems } from "./orders/CreateOrderItems"
-export { DeleteOrderDetails } from "./orders/DeleteOrderDetails";
-export { DeleteOrderItems } from "./orders/DeleteOrderItems"
-export { EditOrderDetails } from "./orders/EditOrderDetails";
-export { EditOrderItems } from "./orders/EditOrderItems"
 export { CreatePaymentDetails}from "./payment/CreatePaymentDetails";
 export { CreateUserPayment }from "./payment/CreateUserPayment";
+export { CreateOrderDetails }from "./payment/CreateOrderDetails";
+export { DeleteOrderDetails } from "./orders/DeleteOrderDetails";
+export { DeleteOrderItems } from "./orders/DeleteOrderItems"
 export { DeleteUserPayment}from "./payment/DeleteUserPayment";
+export { DeletePaymentDetails}from "./payment/DeletePaymentDetails";
+export { EditOrderDetails } from "./orders/EditOrderDetails";
+export { EditOrderItems } from "./orders/EditOrderItems"
 export { EditPaymentDetails}from "./payment/EditPaymentDetails";
 export { EditUserPayment} from "./payment/EditUserPayment";
-
+export { UsersTable } from "./users/UsersTable";
 
 
