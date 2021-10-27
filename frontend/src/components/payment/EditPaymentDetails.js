9f850be18fcf6ad04b9fa752f9868d5e612932cb
Mia <84252026+miadao@users.noreply.github.com>
2021-10-27 14:03:57 -0400

    Added EditPaymentDetails Component

---
+++ frontend/src/components/payment/EditPaymentDetails.js
@@ -0,0 +1,26 @@
+import {edit_payment_details} from "../../api";
+import {useState, useEffect} from "react";
+
+export function EditPaymentDetails() {
+    const [ editPaymentDetails, setEditPaymentDetails ] = useState([])
+        useEffect(() => {
+            const fetchEditPaymentDetails = async () => {
+                const resp = await edit_payment_details() 
+                setEditPaymentDetails(resp.data)
+                console.log(editPaymentDetails)
+            }
+            fetchEditPaymentDetails()
+        },[])
+    return (
+        <>
+            <span> Payment Details Editted</span>
+            { editPaymentDetails && editPaymentDetails.map (editPaymentDetail => {
+                return (
+                    <div className="editPaymentDetail" key={editPaymentDetail.id}> 
+                    <ul> ID: {editPaymentDetail.id} </ul>
+                    </div>
+                )
+            })}
+        </>
+    )
+  }
