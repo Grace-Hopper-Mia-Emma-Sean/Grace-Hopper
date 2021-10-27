7f11b0c72dcdc62faef93d30d96ccda36cd2350e
Mia <84252026+miadao@users.noreply.github.com>
2021-10-27 14:19:40 -0400

    Updated index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -10,3 +10,16 @@ export { PaymentDetails } from "./payment/PaymentDetails";
 export { UserPayment } from "./payment/UserPayment";
 export { CreateOrderDetails } from "./orders/CreateOrderDetails";
 export { UsersTable } from "./users/UsersTable";
+export { CreateOrderItems } from "./orders/CreateOrderItems"
+export { DeleteOrderDetails } from "./orders/DeleteOrderDetails";
+export { DeleteOrderItems } from "./orders/DeleteOrderItems"
+export { EditOrderDetails } from "./orders/EditOrderDetails";
+export { EditOrderItems } from "./orders/EditOrderItems"
+export { CreatePaymentDetails}from "./payment/CreatePaymentDetails";
+export { CreateUserPayment }from "./payment/CreateUserPayment";
+export { DeleteUserPayment}from "./payment/DeleteUserPayment";
+export { EditPaymentDetails}from "./payment/EditPaymentDetails";
+export { EditUserPayment} from "./payment/EditUserPayment";
+
+
+
