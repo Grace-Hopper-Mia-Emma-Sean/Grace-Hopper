2ebb4266aeed5ce537248740c4a82b1ae03409e1
Mia <84252026+miadao@users.noreply.github.com>
2021-10-27 13:48:50 -0400

    Added DeleteOrderDetails component

---
+++ frontend/src/components/orders/DeleteOrderDetails.js
@@ -0,0 +1,27 @@
+import {delete_order_details } from "../../api";
+import {useState, useEffect} from "react";
+
+export function DeleteOrderDetails() {
+    const [ deleteOrderDetails, setDeleteOrderDetails ] = useState([])
+        useEffect(() => {
+            const fetchDeleteOrderDetails = async () => {
+                const resp = await delete_order_details () 
+                console.log(resp)
+                setDeleteOrderDetails(resp.data)
+                console.log(deleteOrderDetails)
+            }
+            fetchDeleteOrderDetails()
+        },[])
+    return (
+        <>
+            <span> Order Details </span>
+            { deleteOrderDetails && deleteOrderDetails.map (deleteOrderDetail => {
+                return (
+                    <div className="order_details" key={deleteOrderDetail.id}> 
+                    <ul> ID: {deleteOrderDetail.id} </ul>
+                    </div>
+                )
+            })}
+        </>
+    )
+  }
