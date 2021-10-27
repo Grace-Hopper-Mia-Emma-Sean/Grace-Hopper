51b4772b1abf627f860791791ece1cdc6b412d56
Mia <84252026+miadao@users.noreply.github.com>
2021-10-27 13:33:57 -0400

    Updated POST, PATCH and DELETE routes.

--- frontend/src/api/orders.js
+++ frontend/src/api/orders.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const order_details = async () => {
   return axios({
     method: "GET",
-    url: "/order-details",
+    url: "/order_details",
     headers: {
       "Content-Type": "application/json",
     },
@@ -14,14 +14,39 @@ const order_details = async () => {
   });
 };
 
-const edit_order_details = async () => {};
+const edit_order_details = async (user_id, payment_id, total) => {
+  return axios({
+      method: "PATCH",
+      url: `/order_details/${orderDetailsId}`,
+      data: {
+        user_id: user_id,
+        payment_id: payment_id,
+        total: total,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).catch((error) => {
+      console.error(error.response.data);
+    });
+};
 
-const delete_order_details = async () => {};
+const delete_order_details = async () => {
+  return axios({
+    method: "DELETE",
+    url: `/order_details/${orderDetailsId}`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).catch((error) => {
+    console.error(error.response.data);
+  });
+};
 
 const create_order_details = async (user_id, payment_id, total) => {
   return axios({
     method: "POST",
-    url: "/order-details",
+    url: "/order_details",
     data: {
       user_id: user_id,
       payment_id: payment_id,
@@ -37,16 +62,56 @@ const create_order_details = async (user_id, payment_id, total) => {
 
 //OrderItems
 
-const edit_order_items = async () => {};
+const edit_order_items = async (order_id, product_id, quantity) => {
+  return axios({
+      method: "PATCH",
+      url: `/order_items/${orderItemsId}`,
+      data: {
+       order_id: order_id,
+      product_id: product_id,
+      quantity: quantity,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).catch((error) => {
+      console.error(error.response.data);
+    });
+};
 
-const delete_order_items = async () => {};
+const delete_order_items = async () => {
+  return axios({
+    method: "DELETE",
+    url: `/order_items/${orderItemsId}`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).catch((error) => {
+    console.error(error.response.data);
+  });
+};
 
-const create_order_items = async () => {};
+const create_order_items = async (order_id, product_id, quantity) => {
+  return axios({
+    method: "POST",
+    url: "/order_items",
+    data: {
+      order_id: order_id,
+      product_id: product_id,
+      quantity: quantity,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).catch((error) => {
+    console.error(error.response.data);
+  });
+};
 
 const order_items = async () => {
   return axios({
     method: "GET",
-    url: "/order-items",
+    url: "/order_items",
     headers: {
       "Content-Type": "application/json",
     },
