e489cf9158c071b32da3c8ddc9626d8eeb351d2f
emma-elise <emma.codes7732@gmail.com>
2021-10-30 07:28:34 -0400

    misc

--- frontend/src/components/cart/CreateCartItem.js
+++ frontend/src/components/cart/CreateCartItem.js
@@ -5,14 +5,14 @@ import { useState, useEffect } from "react";
 export function CreateCartItem() {
   const [cart, setCart] = useState([]);
 
-  useEffect(async () => {
-    await createCartItem(productId, quantity, userId)
-      .then(() => {
-        setCart([]);
-      })
-      .catch((error) => console.log(error))
-      .finally(localStorage.removeItem("cart"));
-  }, []);
+  // useEffect(async () => {
+  //   await createCartItem(productId, quantity, userId)
+  //     .then(() => {
+  //       setCart([]);
+  //     })
+  //     .catch((error) => console.log(error))
+  //     .finally(localStorage.removeItem("cart"));
+  // }, []);
 
   return (
     <div>
