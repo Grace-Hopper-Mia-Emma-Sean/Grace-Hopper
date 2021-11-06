d02619c3087c85f12640831c36deb9e3eb222638
emma-elise <70772019+emma-elise@users.noreply.github.com>
2021-11-06 17:21:03 -0400

    Update order_details.js

--- backend/api/routers/order_details.js
+++ backend/api/routers/order_details.js
@@ -16,7 +16,7 @@ orderDetailsRouter.post("/", async (req, res, next) => {
   const { user_id, total,  payment_id  } = req.body;
   try {
     const createdOrderDetails = await createOrderDetails({user_id, payment_id, total});
-    res.status(200).send({user_id: req.body.user_id, total: req.body.total, payment_id: req.body.total});
+    res.status(200).send({user_id, total, payment_id});
   } catch (error) {
     next(error);
   }
